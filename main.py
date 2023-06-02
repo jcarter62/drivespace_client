35afda71a857ed501130079c076bddc8a1cb4d65
jcarter <Running-97-miles>
2023-06-02 11:52:12 -0700

    Initial Commit

--- main.py
+++ main.py
@@ -1,69 +1,5 @@
 import argparse
-import win32api, win32file, win32con
 import requests
-
-def get_drives() -> list:
-    result = []
-    drives = win32api.GetLogicalDriveStrings()
-    drives = drives.split('\000')[:-1]
-    for drive in drives:
-        drivetype = get_drive_type(drive)
-        driveinfo = get_drive_space(drive)
-        result.append({
-            "drive": drive,
-            "type": drivetype,
-            "info": driveinfo
-        })
-    return result
-
-
-def get_drive_space(drive: str) -> dict:
-    drivenums = win32api.GetDiskFreeSpaceEx(drive)
-    free = bytes_to_gb(drivenums[0])
-    total = bytes_to_gb(drivenums[1])
-    used = total - free
-
-
-    return {
-        "name": get_drive_name(drive),
-        "serial": get_drive_serial(drive),
-        "filesystem": get_drive_filesystem(drive),
-        "free": free,
-        "total": total,
-        "used": used,
-        "freepct": round(free / total * 100, 2),
-        "usedpct": round(used / total * 100, 2),
-    }
-
-def bytes_to_gb(bytes: int) -> float:
-    gb = bytes / 1024 / 1024 / 1024
-    return round(gb, 2)
-
-
-def get_drive_type(drive: str) -> str:
-    drivetype = win32file.GetDriveType(drive)
-    if drivetype == win32con.DRIVE_FIXED:
-        return "Fixed"
-    elif drivetype == win32con.DRIVE_CDROM:
-        return "CD-ROM"
-    elif drivetype == win32con.DRIVE_REMOVABLE:
-        return "Removable"
-    elif drivetype == win32con.DRIVE_REMOTE:
-        return "Remote"
-    elif drivetype == win32con.DRIVE_RAMDISK:
-        return "RAM Disk"
-    else:
-        return "Unknown"
-
-
-def get_drive_name(drive: str) -> str:
-    return win32api.GetVolumeInformation(drive)[0]
-
-def get_drive_serial(drive: str) -> str:
-    return win32api.GetVolumeInformation(drive)[1]
-
-def get_drive_filesystem(drive: str) -> str:
-    return win32api.GetVolumeInformation(drive)[4]
 
 
 # Press the green button in the gutter to run the script.
@@ -71,18 +7,47 @@ if __name__ == '__main__':
     # get api from app arguments
     argp = argparse.ArgumentParser()
     argp.add_argument("--api", type=str, required=True)
+    argp.add_argument("--input", type=str, required=True)
     args = argp.parse_args()
     api = args.api
+    inputfile = args.input
     #
 
-    # get system info
-    drives = get_drives()
     systeminfo = {
-        "name": win32api.GetComputerName(),
-        "domain": win32api.GetDomainName(),
-        "drives": drives,
+        "name": "",
+        "drives": [],
     }
-    #
+
+    # get system info from input file
+    with open(inputfile, "r") as f:
+        # get system name from first line
+        systeminfo["name"] = f.readline().strip()
+        systeminfo['domain'] = f.readline().strip()
+        # get drive information lines
+        driveinfo = []
+        for line in f.readlines():
+            if line.startswith("    "):
+                drv = line.strip()
+                # driveinfo.append(line.strip())
+                totalGB = float(drv[42:53])
+                freeGB = float(drv[56:64])
+                used = totalGB - freeGB
+                usedpct = round(used / totalGB * 100, 2)
+                freepct = round(freeGB / totalGB * 100, 2)
+                driveinfo.append({
+                    "drive": drv[0:1].strip(),
+                    "type": drv[3:13].strip(),
+                    "format": drv[14:20].strip(),
+                    "name": drv[25:35].strip(),
+                    "total": totalGB,
+                    "free": freeGB,
+                    "used": used,
+                    "usedpct": usedpct,
+                    "freepct": freepct,
+                    "serial": "",
+                })
+
+    systeminfo["drives"] = driveinfo
 
     # send system info to api
     requests.post(api, json=systeminfo)
